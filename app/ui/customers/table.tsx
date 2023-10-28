9fef46f1dcc1ab609ef62dfb84f106bd15bbf6dd
kanoi-y <furafuratonbo@gmail.com>
2023-10-28 17:51:33 +0900

    ✨ fontとImageの設定を実装

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from "@/app/ui/fonts";
 import Search from '../search';
 import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
 
