d4ffe222250c726dc9e45ff46d944b42094f542e
kanoi-y <furafuratonbo@gmail.com>
2023-11-12 22:57:02 +0900

    ✨ 認証を実装

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,7 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+import { signIn } from '@/auth';
 
 export type State = {
   errors?: {
@@ -101,5 +102,19 @@ export async function deleteInvoice(id: string) {
     return { message: "Deleted Invoice." };
   } catch (error) {
     return { message: "Database Error: Failed to Delete Invoice." };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
   }
 }
